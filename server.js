8a10f6684dc699c4b5500c38387786462d0e05a0
jcsuazo <jsuazo9@gmail.com>
2020-07-07 10:37:36 -0400

    add basic bootcamps routes

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show a bootcamp ${req.params.id}` });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 app.listen(
   PORT,
   console.log(
