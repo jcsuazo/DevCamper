66e49b79306f81dc904d4d6e0d7b45bf62e9ed47
jcsuazo <jsuazo9@gmail.com>
2020-07-07 15:31:55 -0400

    create db configuration file and connect to mongodb atlas

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ require('dotenv').config({
 });
 const express = require('express');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
 
 const app = express();
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -14,9 +19,16 @@ app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server Running in ${process.env.NODE_ENV} at http://localhost:${PORT}`,
   ),
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
